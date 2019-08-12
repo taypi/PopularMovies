5fc6422d3b59ac95094ac5aaa7253e73667f6ef2
Tay <taypih@gmail.com>
2019-08-11 23:52:41 -0400

    Add poster to adapter

--- app/src/main/java/com/example/popularmovies/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/popularmovies/adapters/MovieAdapter.java
@@ -1,13 +1,18 @@
 package com.example.popularmovies.adapters;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.popularmovies.R;
 import com.example.popularmovies.models.Movie;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -18,27 +23,37 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
     public MovieAdapter() {
     }
 
-    public MovieAdapter(ArrayList<Movie> movieData) {
-        mMovieData = movieData;
-    }
-
     public void setMovieData(ArrayList<Movie> movieData) {
-        this.mMovieData = movieData;
+        mMovieData = movieData;
         notifyDataSetChanged();
     }
 
     @Override
+    @NonNull
     public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.movie_list_item, parent, false);
-        MovieViewHolder viewHolder = new MovieViewHolder(view);
-        return viewHolder;
+        return new MovieViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(MovieViewHolder holder, int position) {
-        holder.mMovieItemTextView.setText(mMovieData.get(position).getTitle());
+    public void onBindViewHolder(MovieViewHolder viewHolder, int position) {
+        Movie movie = mMovieData.get(position);
+        viewHolder.mMovieItemTextView.setText(movie.getTitle());
+        Picasso.get()
+                .load("https://image.tmdb.org/t/p/" + "/w342/" + movie.getPosterPath())
+                .placeholder(R.mipmap.ic_launcher)
+                .into(viewHolder.mPoster, new Callback() {
+                    @Override
+                    public void onSuccess() {
 
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 
     @Override
@@ -48,10 +63,12 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
 
     public static class MovieViewHolder extends RecyclerView.ViewHolder {
         public final TextView mMovieItemTextView;
+        public final ImageView mPoster;
 
         public MovieViewHolder(View view) {
             super(view);
-            mMovieItemTextView = view.findViewById(R.id.list_item_tv);
+            mMovieItemTextView = view.findViewById(R.id.tv_list_item);
+            mPoster = view.findViewById(R.id.iv_list_item);
         }
     }
 
