da7549cb51a25061b439f82318381c2a253662d9
Tay <taypih@gmail.com>
2019-08-30 20:47:49 -0400

    Create MovieDao

---
+++ app/src/main/java/com/example/popularmovies/database/MovieDao.java
@@ -0,0 +1,26 @@
+package com.example.popularmovies.database;
+
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.popularmovies.models.Movie;
+
+import java.util.List;
+
+public interface MovieDao {
+
+    @Query("SELECT * FROM favorite_movie ORDER BY mAverageVote")
+    List<Movie> getFavoriteMovies();
+
+    @Insert
+    void insertFavoriteMovie(Movie movie);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateFavoriteMovie(Movie movie);
+
+    @Delete
+    void deleteFavoriteMovie(Movie movie);
+}
