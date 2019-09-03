901f727ea8a61b671fad2f2a10938e5260751826
Tay <taypih@gmail.com>
2019-09-02 22:46:23 -0400

    Remove type converter

--- app/src/main/java/com/example/popularmovies/database/MovieDatabase.java
+++ app/src/main/java/com/example/popularmovies/database/MovieDatabase.java
@@ -5,12 +5,10 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-import androidx.room.TypeConverters;
 
 import com.example.popularmovies.models.Movie;
 
 @Database(entities = {Movie.class}, version = 1, exportSchema = false)
-@TypeConverters({Converters.class})
 public abstract class MovieDatabase extends RoomDatabase {
     private static final String DATABASE_NAME = "favorite";
     private static MovieDatabase sInstance;
